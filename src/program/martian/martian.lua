195a2c8a392a5ecf2874b07515db5e135e34784d
Diego Pino Garcia <dpino@igalia.com>
2017-11-21 13:13:58 +0100

    Filter martian packets

--- src/program/martian/martian.lua
+++ src/program/martian/martian.lua
@@ -20,7 +20,9 @@ function matches_network(src, dst)
    assert(cidr > 0 and cidr <= 32, "Not valid cidr: "..cidr)
    network_ip = assert(ipv4:pton(network_ip),
       "Not valid network IP address: "..network_ip)
-   src = assert(ipv4:pton(src), "Not valid IP address: "..src)
+   if type(src) == "string" then
+      src = assert(ipv4:pton(src), "Not valid IP address: "..src)
+   end
    local mask = lshift(2^(cidr+1)-1, 32-cidr)
    return band(touint32(src), mask) == touint32(network_ip)
 end
@@ -28,7 +30,37 @@ end
 local MartianFilter = {}
 
 function MartianFilter.new ()
-   return setmetatable({}, {__index=MartianFilter})
+   local o = {
+      -- See https://en.wikipedia.org/wiki/Martian_packet.
+      filtered_networks = {
+         "0.0.0.0/8",
+         "10.0.0.0/8",
+         "100.64.0.0/10",
+         "127.0.0.0/8",
+         "127.0.53.53/32",
+         "169.254.0.0/16",
+         "172.16.0.0/12",
+         "192.0.0.0/24",
+         "192.0.2.0/24",
+         "192.168.0.0/16",
+         "198.18.0.0/15",
+         "198.51.100.0/24",
+         "203.0.113.0/24",
+         "224.0.0.0/4",
+         "240.0.0.0/4",
+         "255.255.255.255/32",
+      }
+   }
+   return setmetatable(o, {__index=MartianFilter})
+end
+
+function MartianFilter:is_martian (ip)
+   for _, each in ipairs(self.filtered_networks) do
+      if matches_network(ip, each) then
+         return true
+      end 
+   end
+   return false
 end
 
 function MartianFilter:push ()
@@ -36,7 +68,11 @@ function MartianFilter:push ()
 
    while not link.empty(input) do
       local pkt = link.receive(input)
-      link.transmit(output, pkt)
+      local ip_hdr = ipv4:new_from_mem(pkt.data + 14, 20)
+      local ret = self:is_martian(ip_hdr:src())
+      if not self:is_martian(ip_hdr:src()) then
+         link.transmit(output, pkt)
+      end
    end
 end
 
