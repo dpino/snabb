82c3b87de052e7136d4a1350576582da6d94127a
Diego Pino Garcia <dpino@igalia.com>
2017-12-24 17:13:46 +0100

    Print help

--- src/program/dnssd/dnssd.lua
+++ src/program/dnssd/dnssd.lua
@@ -1,6 +1,29 @@
 module(..., package.seeall)
 
-function run()
+local lib = require("core.lib")
+
+local long_opts = {
+   help = "h",
+}
+
+local function usage(exit_code)
+   print(require("program.dnssd.README_inc"))
+   main.exit(exit_code)
+end
+
+function parse_args (args)
+   local handlers = {}
+   function handlers.h (arg)
+      usage(0)
+   end
+   args = lib.dogetopt(args, handlers, "hl:", long_opts)
+   if #args > 1 then usage(1) end
+   return args
+end
+
+function run(args)
+   local iface = parse_args(args)
+
    print("dnssd")
 end
 
