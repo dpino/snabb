8a68b512b0eead9ef5d7aeda6991ecc650e47ce5
Diego Pino Garcia <dpino@igalia.com>
2017-12-27 08:17:38 +0100

    Add feature to read incoming packets from pcap file

--- src/program/dnssd/dnssd.lua
+++ src/program/dnssd/dnssd.lua
@@ -6,9 +6,12 @@ local RawSocket = require("apps.socket.raw").RawSocket
 local basic_apps = require("apps.basic.basic_apps")
 local ffi = require("ffi")
 local lib = require("core.lib")
+local pcap = require("apps.pcap.pcap")
 
 local long_opts = {
    help = "h",
+   pcap = "p",
+   interface = "i",
 }
 
 local function usage(exit_code)
@@ -17,13 +20,38 @@ local function usage(exit_code)
 end
 
 function parse_args (args)
+   local function fexists (filename)
+      local fd = io.open(filename, "r")
+      if fd then
+         fd:close()
+         return true
+      end
+      return false
+   end
+   local opts = {}
    local handlers = {}
    function handlers.h (arg)
       usage(0)
    end
-   args = lib.dogetopt(args, handlers, "hl:", long_opts)
-   if #args > 1 then usage(1) end
-   return args
+   function handlers.p (arg)
+      opts.pcap = arg
+   end
+   function handlers.i (arg)
+      opts.interface = arg
+   end
+   args = lib.dogetopt(args, handlers, "hp:i:", long_opts)
+   if opts.pcap or opts.interface then
+      assert(#args == 0, function() usage(1) end)
+   else
+      assert(#args == 1, function() usage(1) end)
+      local filename = args[1]
+      if fexists(filename) then
+         opts.pcap = filename
+      else
+         opts.interface = filename
+      end
+   end
+   return opts, args
 end
 
 DNSSD = {}
@@ -82,17 +110,23 @@ function DNSSD:log (pkt)
 end
 
 function run(args)
-   args = parse_args(args)
-   local iface = assert(args[1])
-
-   print(("Capturing packets from interface '%s'"):format(iface))
+   local opts, args = parse_args(args)
 
    local c = config.new()
-   config.app(c, "iface", RawSocket, iface)
    config.app(c, "dnssd", DNSSD)
    -- config.link(c, "dnssd.output -> iface.rx")
-   config.link(c, "iface.tx -> dnssd.input")
-
+   if opts.pcap then
+      print("Reading from file: "..opts.pcap)
+      config.app(c, "pcap", pcap.PcapReader, opts.pcap)
+      config.link(c, "pcap.output-> dnssd.input")
+   elseif opts.interface then
+      local iface = opts.interface
+      print(("Capturing packets from interface '%s'"):format(iface))
+      config.app(c, "iface", RawSocket, iface)
+      config.link(c, "iface.tx -> dnssd.input")
+   else
+      error("Unreachable")
+   end
    engine.configure(c)
    engine.main({report = {showapps = true, showlinks = true}})
 end
