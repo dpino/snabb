68583da79ee2e5c833f4bd13f74ff14ab0bb8d20
Diego Pino Garcia <dpino@igalia.com>
2018-01-05 12:06:33 +0100

    Improvements on DNSSD subcommand
    - Improve option parsing.
    - Fetch ethernet address and ipv4 addres from network interface.
    - Clean up code.

--- src/program/dnssd/dnssd.lua
+++ src/program/dnssd/dnssd.lua
@@ -8,6 +8,8 @@ local ffi = require("ffi")
 local lib = require("core.lib")
 local pcap = require("apps.pcap.pcap")
 
+local mDNSQuery = require("program.dnssd.lib.mdns_query").mDNSQuery
+
 local long_opts = {
    help = "h",
    pcap = "p",
@@ -40,27 +42,32 @@ function parse_args (args)
       opts.interface = arg
    end
    args = lib.dogetopt(args, handlers, "hp:i:", long_opts)
-   if opts.pcap or opts.interface then
-      if #args ~= 0 then usage(1) end
-   else
-      if #args ~= 1 then usage(1) end
+   if not (opts.pcap or opts.interface) then
       local filename = args[1]
       if fexists(filename) then
          opts.pcap = filename
       else
          opts.interface = filename
       end
+      table.remove(args, 1)
    end
    return opts, args
 end
 
 DNSSD = {}
 
-function DNSSD.new (args)
+function DNSSD:new (args)
    local o = {
-      interval = 2, -- Delay between broadcast messages.
+      interval = 1, -- Delay between broadcast messages.
       threshold = 0,
    }
+   if args then
+      o.requester = mDNSQuery.new({
+         src_eth = assert(args.src_eth),
+         src_ipv4 = assert(args.src_ipv4),
+      })
+      o.query = args.query or "_services._dns-sd._udp.local"
+   end
    return setmetatable(o, {__index = DNSSD})
 end
 
@@ -69,15 +76,12 @@ function DNSSD:pull ()
    local output = self.output.output
    if not output then return end
 
-   -- while not link.full(output) do
-      local now = os.time()
-      if now > self.threshold then
-         self.threshold = now + self.interval
-         -- Send packet.
-         local pkt = MDNS.query()
-         link.transmit(output, pkt)
-      end
-   -- end
+   local now = os.time()
+   if now > self.threshold then
+      self.threshold = now + self.interval
+      local pkt = self.requester:build(self.query)
+      link.transmit(output, pkt)
+   end
 end
 
 function DNSSD:push ()
@@ -87,8 +91,6 @@ function DNSSD:push ()
       local pkt = link.receive(input)
       if MDNS.is_mdns(pkt) then
          self:log(pkt)
-      else
-         -- print("not mdns")
       end
       packet.free(pkt)
    end
@@ -111,24 +113,52 @@ function DNSSD:log (pkt)
    end
 end
 
+local function execute (cmd)
+   local fd = assert(io.popen(cmd, 'r'))
+   local ret = fd:read("*all")
+   fd:close()
+   return ret
+end
+
+local function chomp (str)
+   return str:gsub("\n", "")
+end
+
+local function ethernet_address_of (iface)
+   local cmd = ("ip li sh %s | grep 'link/ether' | awk '{print $2}'"):format(iface)
+   return chomp(execute(cmd))
+end
+
+local function ipv4_address_of (iface)
+   local cmd = ("ip addr sh %s | grep 'inet ' | awk '{print $2}'"):format(iface)
+   local output = chomp(execute(cmd))
+   local pos = output:find("/")
+   return pos and output:sub(0, pos-1) or output
+end
+
 function run(args)
    local opts, args = parse_args(args)
 
    local duration
    local c = config.new()
-   config.app(c, "dnssd", DNSSD)
    if opts.pcap then
       print("Reading from file: "..opts.pcap)
+      config.app(c, "dnssd", DNSSD)
       config.app(c, "pcap", pcap.PcapReader, opts.pcap)
       config.link(c, "pcap.output-> dnssd.input")
-      duration = 4
+      duration = 3
    elseif opts.interface then
       local iface = opts.interface
+      local query = args[1]
       print(("Capturing packets from interface '%s'"):format(iface))
+      config.app(c, "dnssd", DNSSD, {
+         src_eth = ethernet_address_of(iface),
+         src_ipv4 = ipv4_address_of(iface),
+         query = query,
+      })
       config.app(c, "iface", RawSocket, iface)
       config.link(c, "iface.tx -> dnssd.input")
       config.link(c, "dnssd.output -> iface.rx")
-      duration = 10
    else
       error("Unreachable")
    end
