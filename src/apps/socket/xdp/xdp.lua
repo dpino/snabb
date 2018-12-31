73060fc94fd5f42e063fb4b16d914a3ab55c4767
Diego Pino Garcia <dpino@igalia.com>
2018-12-31 10:29:52 +0100

    Add XDP Socket app

---
+++ src/apps/socket/xdp/xdp.lua
@@ -0,0 +1,112 @@
+-- Use of this source code is governed by the Apache 2.0 license; see COPYING.
+
+module(...,package.seeall)
+
+local S = require("syscall")
+
+local ffi = require("ffi")
+local C = ffi.C
+
+require("apps.socket.xdp.xdpsock_app_h")
+
+XDPSocket = {}
+XDPSocket.__index = XDPSocket
+
+function XDPSocket:new (ifname)
+   assert(ifname)
+
+   local ret = C.init_xdp(ifname) 
+   if not ret then
+      print("Error initializing XDP Socket in "..ifname)
+      os.exit(1)
+   end
+
+   local o = {
+      rx_p = packet.allocate(),
+      dev = {
+         context = ret,
+         can_receive = C.can_receive,
+         can_transfer = C.can_transfer,
+         receive = C.receive,
+         transfer = C.transfer,
+      }
+   }
+   return setmetatable(o, {__index = XDPSocket})
+end
+
+function XDPSocket:tx_only ()
+   return self.dev.tx_only()
+end
+
+function XDPSocket:rx_drop_all ()
+   return self.dev.rx_drop_all()
+end
+
+function XDPSocket:can_receive ()
+   return self.dev.can_receive(self.dev.context)
+end
+
+function XDPSocket:receive ()
+   local p = self.rx_p
+   local len = self.dev.receive(self.dev.context, p.data)
+   p.length = len
+   return p
+end
+
+function XDPSocket:pull ()
+   local tx = self.input and self.input.tx
+   if not tx then return end
+   local limit = engine.pull_npackets
+   while limit > 0 and self:can_receive() do
+      limit = limit - 1
+      link.transmit(tx, self:receive())
+   end
+end
+
+function XDPSocket:can_transfer()
+   return self.dev.can_transfer(self.dev.context)
+end
+
+function XDPSocket:transfer (p)
+   return self.dev.transfer(self.dev.context, p.data, p.length)
+end
+
+function XDPSocket:push ()
+   local rx = self.input and self.input.rx
+   if not rx then return end
+   while not link.empty(rx) and self:can_transfer() do
+      local p = link.receive(rx)
+      self:transfer(p)
+      packet.free(p)
+   end
+end
+
+function selftest ()
+   print("selftest:")
+   local lib = require("core.lib")
+
+   local if_name = lib.getenv("SNABB_PCI0")
+   if not if_name then
+      print("skipped")
+      return main.exit(engine.test_skipped_code)
+   end
+
+   local xdp = XDPSocket:new(if_name)
+   xdp.input = {
+      rx = link.new("l_input")
+   }
+   xdp.output = {
+      tx = link.new("l_output")
+   }
+   local pkt = packet.from_string(lib.hexundump([=[
+   3c fd fe 9e 7f 71 ec b1 d7 98 3a c0 08 00 45 00
+   00 2e 00 00 00 00 40 11 88 97 05 08 07 08 c8 14
+   1e 04 10 92 10 92 00 1a 6d a3 34 33 1f 69 40 6b
+   54 59 b6 14 2d 11 44 bf af d9 be aa
+   ]=], 60))
+   link.transmit(xdp.input.rx, pkt)
+   xdp:push()
+   link.transmit(xdp.output.tx, pkt)
+   xdp:pull()
+   print("ok")
+end
---
+++ src/apps/socket/xdp/xdpsock_app.h
@@ -0,0 +1,77 @@
+// Use of this source code is governed by the Apache 2.0 license; see COPYING.
+
+/**
+ * Part of this code is based on XXX.
+ */
+
+enum benchmark_type {
+    BENCH_RXDROP = 0,
+    BENCH_TXONLY = 1,
+    BENCH_L2FWD = 2,
+};
+
+struct xdp_umem_uqueue {
+    uint32_t cached_prod;
+    uint32_t cached_cons;
+    uint32_t mask;
+    uint32_t size;
+    uint32_t *producer;
+    uint32_t *consumer;
+    uint64_t *ring;
+    void *map;
+};
+
+struct xdp_umem {
+    char *frames;
+    struct xdp_umem_uqueue fq;
+    struct xdp_umem_uqueue cq;
+    int fd;
+};
+
+struct xdp_uqueue {
+    uint32_t cached_prod;
+    uint32_t cached_cons;
+    uint32_t mask;
+    uint32_t size;
+    uint32_t *producer;
+    uint32_t *consumer;
+    struct xdp_desc *ring;
+    void *map;
+};
+
+struct xdpsock {
+    struct xdp_uqueue rx;
+    struct xdp_uqueue tx;
+    int sfd;
+    struct xdp_umem *umem;
+    uint32_t outstanding_tx;
+    unsigned long rx_npkts;
+    unsigned long tx_npkts;
+    unsigned long prev_rx_npkts;
+    unsigned long prev_tx_npkts;
+};
+
+struct options {
+    int bench;
+    char opt_if[256];
+    int ifindex;
+    int poll;
+    int queue;
+    int xdp_bind_flags;
+    int xdp_flags;
+};
+
+typedef struct options options_t;
+
+typedef struct xdp_context {
+    struct xdpsock *xsks[4];
+	int num_socks;
+	struct pollfd *fds_in;
+	struct pollfd *fds_out;
+} xdp_context_t;
+
+bool can_receive(xdp_context_t* ctx);
+bool can_transfer(xdp_context_t *ctx);
+int receive(xdp_context_t* ctx, char* pkt);
+int transfer(xdp_context_t *ctx, const char* data, size_t length);
+xdp_context_t* init_xdp(const char *ifname);
