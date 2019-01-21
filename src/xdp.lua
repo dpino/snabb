f859ea075cd988b3ed52ab4615beda68e9c377b8
Diego Pino Garcia <dpino@igalia.com>
2019-01-21 13:09:13 +0100

    Update XDP test script

--- src/xdp.lua
+++ src/xdp.lua
@@ -5,6 +5,51 @@ local lib = require("core.lib")
 local XDPSocket = require("apps.socket.xdp.xdp").XDPSocket 
 local basic_apps = require("apps.basic.basic_apps")
 
+local function test_one_nic (pkt)
+   local c = config.new()
+   config.app(c, "source", basic_apps.Source)
+   config.app(c, "tee", basic_apps.Tee)
+   config.app(c, "nic0", XDPSocket, "veth0")
+
+   config.link(c, "source.tx -> tee.rx")
+   config.link(c, "tee.tx -> nic0.rx")
+
+   engine.configure(c)
+   engine.app_table.source:set_packet(pkt)
+
+   engine.main({duration=1, report={showlinks=true}})
+end
+
+local function test_two_nics (pkt)
+   local c = config.new()
+   config.app(c, "source", basic_apps.Source)
+   config.app(c, "tee", basic_apps.Tee)
+   config.app(c, "nic0", XDPSocket, "veth0")
+   config.app(c, "nic1", XDPSocket, "veth1")
+   config.app(c, "sink", basic_apps.Sink)
+
+   config.link(c, "source.tx -> tee.rx")
+   config.link(c, "tee.tx -> nic0.rx")
+   config.link(c, "nic1.tx -> sink.rx")
+
+   engine.configure(c)
+   engine.app_table.source:set_packet(pkt)
+
+   engine.main({duration=0.1, report={showlinks=true}})
+end
+
+local function test_one_nic_simple (pkt)
+   local c = config.new()
+   config.app(c, "nic1", XDPSocket, "veth1")
+   config.app(c, "sink", basic_apps.Sink)
+
+   config.link(c, "nic1.tx -> sink.rx")
+
+   engine.configure(c)
+
+   engine.main({duration=0.1, report={showlinks=true}})
+end
+
 function selftest()
    print("selftest")
 
@@ -15,47 +60,6 @@ function selftest()
       54 59 b6 14 2d 11 44 bf af d9 be aa
    ]=], 60))
 
-   local c = config.new()
-   config.app(c, "source", basic_apps.Source)
-   config.app(c, "tee", basic_apps.Tee)
-   config.app(c, "nic0", XDPSocket, "veth0")
-   config.app(c, "sink", basic_apps.Sink)
-
-   config.link(c, "source.tx -> tee.rx")
-   config.link(c, "tee.tx -> nic0.rx")
-
-   engine.configure(c)
-   engine.app_table.source:set_packet(pkt)
-
-   engine.main({duration=1, report={showlinks=true}})
-
-   --[[
-   local c = config.new()
-   config.app(c, "source", basic_apps.Source)
-   config.app(c, "tee", basic_apps.Tee)
-   config.app(c, "nic0", XDPSocket, "veth0")
-   config.app(c, "nic1", XDPSocket, "veth1")
-   config.app(c, "sink", basic_apps.Sink)
-
-   config.link(c, "source.tx -> tee.rx")
-   config.link(c, "tee.tx -> nic0.rx")
-   config.link(c, "nic1.tx -> sink.rx")
-
-   engine.configure(c)
-   engine.app_table.source:set_packet(pkt)
-
-   engine.main({duration=0.1, report={showlinks=true}})
-   --]]
-
-   --[[
-   local c = config.new()
-   config.app(c, "nic1", XDPSocket, "veth1")
-   config.app(c, "sink", basic_apps.Sink)
-
-   config.link(c, "nic1.tx -> sink.rx")
-
-   engine.configure(c)
-
-   engine.main({duration=0.1, report={showlinks=true}})
-   --]]
+   -- test_two_nics(pkt)
+   test_one_nic(pkt)
 end
